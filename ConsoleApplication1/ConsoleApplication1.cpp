437386de9812ca82a53ff90923d1f64b448105a6
Ekaterina Ogilko <ogilko.katya@mail.ru>
2024-12-23 14:42:09 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <limits>
+
+int main() {
+    const char* filename = "C:\\Users\\ogilk\\Desktop\\321_Ogilko_Ekaterina\\ConsoleApplication1\\numbers.txt";
+
+    std::ifstream inFile(filename);
+    if (!inFile.is_open()) {
+        std::cerr << "Error when opening file." << std::endl;
+        return 1;
+    }
+
+    std::string number;
+    int maxNumber = std::numeric_limits<int>::min();
+
+    while (inFile >> number) {
+        try {
+            int num = std::stoi(number);
+            if (num > maxNumber) {
+                maxNumber = num;
+            }
+        }
+        catch (const std::invalid_argument&) {
+            std::cerr << "Incorrect number: " << number << std::endl;
+        }
+    }
+
+    inFile.close();
+
+    std::ofstream outFile(filename, std::ios::app); 
+    if (outFile.is_open()) {
+        outFile << "\nMax number: " << maxNumber << std::endl;
+        outFile.close();
+    }
+    else {
+        std::cerr << "Error." << std::endl;
+        return 1;
+    }
+
+    std::cout << "Max number: " << maxNumber << std::endl;
+
+    return 0;
+}
